da90193da918ea9c3a328b156e1ec9678d7a739b
ejaquay <ejjaquay@gmail.com>
2022-12-20 23:23:31 -0500

    Add more comments, remove some blank code lines

--- endings.c
+++ endings.c
@@ -1,4 +1,5 @@
-// Determine line ending types for files in directory tree
+// Determine line ending types for files in a directory tree.
+// Should work for ascii and UTF-8 encoded text, others not.
 
 #include <windows.h>
 #include <string.h>
@@ -7,7 +8,7 @@
 #include <shlwapi.h>
 #pragma comment(lib, "Shlwapi.lib")
 
-int checkfile(const char *);
+int checkendings(const char *);
 int scandir(const char *,const char *);
 int istypebin(const char *);
 
@@ -35,17 +36,15 @@ int scandir(const char *path, const char *match)
     char buf[MAX_PATH];
 
     DWORD attr = GetFileAttributes(path);                    // Check path
-
     if (attr & FILE_ATTRIBUTE_DIRECTORY) {                   // If path a directory
         strcpy_s(buf, MAX_PATH, path);                       //   Set up wildcard
         strcat_s(buf, MAX_PATH, "\\*");
         h = FindFirstFile(buf, &f);                          //   Find first file
         if (h == INVALID_HANDLE_VALUE) return 0;             //   Abort on error
-    } else {                                                 // Else
-        checkfile(path);                                     //   Just check file
+    } else {                                                 // Else not dir
+        checkendings(path);                                  //   check endings
         return 1;                                            //   and return
     }
-
     do {                                                     // Do the rest
         strcpy_s(buf, MAX_PATH, path);                       //   Prepend path to filename
         strcat_s(buf, MAX_PATH, "\\");
@@ -57,9 +56,10 @@ int scandir(const char *path, const char *match)
             scandir(buf,match);                              //   Recurse
         } else {                                             // Else
             if (f.nFileSizeLow == 0) continue;               //   Skip empty files
-            if (match != NULL)                               //   Skip obvious binaries
-                if (! PathMatchSpec(f.cFileName,match)) continue;
-            checkfile(buf);                                  //   Check endings
+            if (match != NULL)                               //   If a match spec
+                if (! PathMatchSpec(f.cFileName,match))      //     Skip unmatched files
+                    continue;
+            checkendings(buf);                               //   Check line endings
         }
     } while (FindNextFile(h, &f));                           // Next file
 
@@ -67,7 +67,7 @@ int scandir(const char *path, const char *match)
     return 1;
 }
 
-int checkfile(const char *file)
+int checkendings(const char *file)
 {
     FILE *in;
     int num_cr = 0;
@@ -75,47 +75,39 @@ int checkfile(const char *file)
     int num_crlf = 0;
     int last_cr = 0;
 
-    // Skip obvious binary files.
-    if (istypebin(file)) return 0;
+    if (istypebin(file)) return 0;          // Skip obvious binary files.
+    in = fopen(file, "rb");                 // Open file for bin read
+    if (in == NULL) return 0;               // Abort if open failed
 
-    // Open file for bin read
-    in = fopen(file, "rb");
-    if (in == NULL) return 0;
-
-    // Count lines and line ending characters
-    int ch;
+    int ch;                                 // Count CR, LF, and CRLF
     while ((ch = fgetc(in)) != EOF) {
         switch (ch) {
-        case '\r':    // CR
-            num_cr++;
+        case '\r':                          // CR found
+            num_cr++;                       // Count the CR
             last_cr = 1;
             break;
-        case '\n':    // LF
-            if (last_cr) {
-                num_crlf++;
-                num_cr--;
-            } else {
-                num_lf++;
+        case '\n':                          // LF found
+            if (last_cr) {                  // If last chr was CR
+                num_crlf++;                 //   Count as CRLF
+                num_cr--;                   //   Uncount the CR
+            } else {                        // else
+                num_lf++;                   //   Count the LF
             }
         default:
-            last_cr = 0;
+            last_cr = 0;                    // Clear last char CR flag
             break;
         }
     }
+    fclose(in);                              // Close the file
 
-    // close the file
-    fclose(in);
-
-    // Print result for file that appears to be text
-    int lines = num_cr + num_lf + num_crlf;
-    if (num_cr == lines) {
+    int lines = num_cr + num_lf + num_crlf;  // Sum the lines
+    if (num_cr == lines) {                   // Text all lines CR
         printf(" CR    %s\n", file);
-    } else if (num_lf == lines) {
+    } else if (num_lf == lines) {            // Text all lines LF
         printf(" LF    %s\n", file);
-    } else if (num_crlf == lines) {
+    } else if (num_crlf == lines) {          // Text all lines CRLF
         printf(" CRLF  %s\n", file);
     }
-
     return 1;
 }
 
@@ -123,9 +115,7 @@ int istypebin(const char *file)
 {
     char *ext = strrchr(file, '.');
     if (ext == NULL) return 0;
-
-    char *list[] = {
-        ".png",
+    char *list[] = {  // List of types assumed to be binary
         ".obj",
         ".exe",
         ".dll",
@@ -133,6 +123,7 @@ int istypebin(const char *file)
         ".pdb",
         ".sbr",
         ".pdf",
+        ".png",
         ".res",
         ".tlog",
         ".ico",
@@ -150,7 +141,6 @@ int istypebin(const char *file)
         ".ova",
         NULL
     };
-
     int i = 0;
     while(list[i]) if (stricmp(ext,list[i++]) == 0) return 1;
     return 0;
